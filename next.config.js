9ac84d20dcf6c206a24e611a5e81e0095fe78e0c
Shahzaib Kamal <gtskahmed@gmail.com>
2021-11-10 02:22:38 +0500

    migrate env file to next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: "mongodb+srv://shahzaib:Daata321@cluster0.tosar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    MONGODB_DB: "myFirstDatabase"
+  }
 }
