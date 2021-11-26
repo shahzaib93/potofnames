5e797cfaf5eee6c5c5fb2bba18c5ee43b4888a1c
Shahzaib Kamal <gtskahmed@gmail.com>
2021-11-26 16:16:45 +0500

    s

--- pages/index.js
+++ pages/index.js
@@ -107,22 +107,22 @@ export default function Home({participants}) {
     console.log(seg);
   }
 
-  const loginAuth = async event => {
-    event.preventDefault()
-    // router.replace(router.asPath);
-    const res = await fetch('http://localhost:3000/api/login', {
-      body: JSON.stringify({
-        username: event.target.username.value,
-        password: event.target.password.value
-      }),
-      headers: {'Content-Type': 'application/json'},
-      method: 'POST'
-    })
-    const loginReturned = await res.json()
-    console.log(loginReturned.token )
-    const json = jwt.decode(loginReturned.token)
-    console.log(json);
-  }
+  // const loginAuth = async event => {
+  //   event.preventDefault()
+  //   // router.replace(router.asPath);
+  //   const res = await fetch('http://localhost:3000/api/login', {
+  //     body: JSON.stringify({
+  //       username: event.target.username.value,
+  //       password: event.target.password.value
+  //     }),
+  //     headers: {'Content-Type': 'application/json'},
+  //     method: 'POST'
+  //   })
+  //   const loginReturned = await res.json()
+  //   console.log(loginReturned.token )
+  //   const json = jwt.decode(loginReturned.token)
+  //   console.log(json);
+  // }
 
   return (
     <div className="container container-sm container-md mb-5">
