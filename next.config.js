ecac0e4ecc0ac1675b6eba119f2a834e36cc5db8
Shahzaib Kamal <gtskahmed@gmail.com>
2021-11-28 21:49:51 +0500

    x

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     MONGODB_DB: "myFirstDatabase",
     GOOGLE_CLIENT_ID:"316073399872-lt1u9e26afvf4q0jujo2fu2kbgp2vja3.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET:"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",
-    // NEXTAUTH_URL:"https://potofnames.com/"
-    NEXTAUTH_URL:"http://localhost:3000/"
+    NEXTAUTH_URL:"https://potofnames.com/"
+    // NEXTAUTH_URL:"http://localhost:3000/"
   }
 }
