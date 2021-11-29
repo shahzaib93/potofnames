53ae8e5a560beef82e011a3813a5ca198e43c518
Shahzaib Kamal <gtskahmed@gmail.com>
2021-11-29 22:02:48 +0500

    next auth lib changed

--- next.config.js
+++ next.config.js
@@ -6,6 +6,5 @@ module.exports = {
     GOOGLE_CLIENT_ID:"712097391157-k2np9fogv44eq0k015ne7a4f1juohri4.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET:"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",
     NEXTAUTH_URL:"https://potofnames.com/",
-    // NEXTAUTH_URL:"http://localhost:3000/"
   }
 }
