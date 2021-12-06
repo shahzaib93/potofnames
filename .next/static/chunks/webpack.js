b5f2652b44e094d9b83a4b2a1601354b05dd8575
Shahzaib Kamal <gtskahmed@gmail.com>
2021-12-06 12:54:22 +0500

    fast shake speed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89139b835ccec970"; }
+/******/ 		__webpack_require__.h = function() { return "1c295ccf597d122b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
